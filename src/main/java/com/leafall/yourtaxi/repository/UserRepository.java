a479c9c7f209065bf4e0203152ab2ffb170c656a
betrayal96 <vogistv@gmail.com>
2026-05-21 17:15:06 +0300

    add search to employees

--- src/main/java/com/leafall/yourtaxi/repository/UserRepository.java
+++ src/main/java/com/leafall/yourtaxi/repository/UserRepository.java
@@ -5,14 +5,14 @@ import com.leafall.yourtaxi.entity.enums.UserRole;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID>, JpaSpecificationExecutor<UserEntity> {
     Optional<UserEntity> findByEmail(String email);
     Optional<UserEntity> findByRole(UserRole role);
-    Page<UserEntity> findAllByRoleAndDeletedAtIsNull(UserRole role, Pageable pageable);
 }
